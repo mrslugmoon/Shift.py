4c6ed94fdfe5b076df24782cc74d3bdca1304e4e
mrslugmoon <112490774+mrslugmoon@users.noreply.github.com>
2022-10-31 12:22:02 +0000

    Create Shift.py.py

---
+++ Shift.py.py
@@ -0,0 +1,1 @@
+from discord.ext import commands class Hello(commands.Cog): def __init__(self, bot): self.bot = bot @commands.Cog.listener() async def on_message(self, message): print(message.content) @commands.command() async def say(self, ctx, *, message): await ctx.send(message) async def setup(bot): await bot.add_cog(Hello(bot))
